cbfccf5db269d777f3a07c0614b51c58400bb32c
Askar Musakunov <askarmusakunov@Askars-MacBook-Pro.local>
2020-10-16 23:31:59 -0500

    group project

---
+++ src/main/java/application/BankBase.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class BankBase implements Bank{
+
+    private long accountNumber;
+    private long routingNumber;
+
+    public static Set<BankBase> allBankRecords = new HashSet<>();
+
+
+    public BankBase(long accountNumber, long routingNumber) throws Exception {
+        if (Long.toString(accountNumber).length() != 12){
+            throw  new Exception("Account number must be 12");
+        }
+        else if(Long.toString(routingNumber).length() != 9){
+            throw  new Exception("Routing number must be 9");
+        }
+        else {
+            this.accountNumber = accountNumber;
+            this.routingNumber = routingNumber;
+        }
+    }
+
+
+    /**
+     * Adds newly created bank to static list after checking for duplicate account and routing numbers
+     * @param bank
+     * @throws Exception
+     */
+     public static void addToAllBankRecords(BankBase bank) throws Exception {
+         if (!allBankRecords.isEmpty()) {
+             for (BankBase b : allBankRecords) {
+                 if (b.routingNumber == bank.routingNumber) {
+                     throw new Exception("Routing number " + bank.routingNumber + " already exists");
+                 } else if (b.accountNumber == bank.accountNumber) {
+                     throw new Exception("Account number " + bank.accountNumber + " already exists");
+                 } else {
+                     allBankRecords.add(bank);
+                 }
+             }
+         } else {
+             allBankRecords.add(bank);
+         }
+     }
+
+}
